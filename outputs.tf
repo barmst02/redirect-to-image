838f0420947e76c5e2074c62fe1ee39c506f43f8
barmst02 <ba@john146.org>
2026-06-30 15:45:10 -0500

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,30 @@
+# outputs.tf
+output "s3_bucket_name" {
+  description = "Name of the S3 bucket for images"
+  value       = aws_s3_bucket.images.id
+}
+
+output "s3_bucket_arn" {
+  description = "ARN of the S3 bucket"
+  value       = aws_s3_bucket.images.arn
+}
+
+output "lambda_function_name" {
+  description = "Name of the Lambda function"
+  value       = aws_lambda_function.image_service.function_name
+}
+
+output "lambda_function_arn" {
+  description = "ARN of the Lambda function"
+  value       = aws_lambda_function.image_service.arn
+}
+
+output "lambda_function_url" {
+  description = "URL endpoint for the Lambda function"
+  value       = aws_lambda_function_url.image_service.function_url
+}
+
+output "lambda_invoke_example" {
+  description = "Example of how to invoke the Lambda"
+  value       = "${aws_lambda_function_url.image_service.function_url}images/your-image.jpg"
+}
